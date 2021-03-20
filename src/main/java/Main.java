f1b062b97af8c3f1d6c3013a0ae138aaeab7d0b3
Alexandre Zenon <a.zenon@student.uclouvain.be>
2021-03-20 18:37:27 +0100

    ON PEUT OUVRIR UNE FENETRE AAAAHH

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,17 @@
-
-
-import State.Country;
-
-import java.util.List;
+import static com.raylib.Jaylib.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Country country = new Country();
+    public static void main(String args[]) {
+        InitWindow(1920, 1080, "Trump Inc.");
+        SetTargetFPS(60);
+
+        while (!WindowShouldClose()){
+            BeginDrawing();
+            DrawText("Hello world", 190, 200, 20, VIOLET);
+            DrawFPS(20, 20);
+            EndDrawing();
+        }
+
+        CloseWindow();
     }
 }
