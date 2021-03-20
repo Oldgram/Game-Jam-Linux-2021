f91a4fd603386631ccf4ae005f1f94ba6c7d80d3
richard <richard.dejamblinne@student.uclouvain.be>
2021-03-20 18:05:50 +0100

    add Contry = list of stat

---
+++ src/main/java/State/Country.java
@@ -0,0 +1,8 @@
+package State;
+
+public class Country {
+    State Alabama = new State("Alabama",4903185,7,50);
+    State c = new State("Alabama",100,10,50);
+    State d = new State("Alabama",100,10,50);
+    State e = new State("Alabama",100,10,50);
+}
